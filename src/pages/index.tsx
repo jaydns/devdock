38da596bdfa0890d3982729e64b5698bcf61dacb
Ryan Jasin <github@jasin.dev>
2024-01-27 17:10:02 -0500

    try this one

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import ProjectCard from "@/components/ProjectCard";
 
 export default function Home() {
 	return (
-		<main className="h-screen w-screen bg-white dark:bg-black">
+		<main className="h-screen w-screen bg-white dark:bg-[rgba(39,39,39,255)]">
 			<h1>Hello</h1>
 			<div>
 				<ProjectCard></ProjectCard>
