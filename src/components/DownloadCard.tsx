2643360ba4dbf7413c57ba1c1d2e0643a39b7656
Ryan Jasin <github@jasin.dev>
2024-01-28 00:55:39 -0500

    soijfsdfoijdgouhdgfouh

---
+++ src/components/DownloadCard.tsx
@@ -0,0 +1,79 @@
+import {
+	Autocomplete,
+	AutocompleteItem,
+	Button,
+	Card,
+	CardBody,
+	CardHeader,
+	Progress,
+} from "@nextui-org/react";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+function getRandomInt(max: number) {
+	return Math.floor(Math.random() * max);
+}
+
+const versions: Record<string, Array<string>> = {
+	python: ["3.12", "3.11", "3.10", "3.9"],
+	node: ["24", "23", "22", "21"],
+};
+
+export default function DownloadCard(props: { language: string }) {
+	const [disabled, setDisabled] = useState(true);
+	const [progressValue, setProgressValue] = useState(0);
+	const [showProgress, setShowProgress] = useState(false);
+	const [isFinished, setIsFinished] = useState(false);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setProgressValue((v) => (v >= 100 ? 100 : v + getRandomInt(20)));
+		}, 500);
+		setIsFinished(true);
+		setShowProgress(false);
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+	return (
+		<div>
+			<Card className="min-w-64 max-w-64">
+				<CardHeader className="flex flex-col gap-4">
+					<Image
+						className="mx-auto"
+						src={`${props.language.toLowerCase()}.svg`}
+						alt="Language Logo"
+						width={`${props.language.toLowerCase() === "python" ? 80 : "node" ? 80 : 100}`}
+						height={`${props.language.toLowerCase() === "python" ? 80 : "node" ? 80 : 100}`}
+					/>
+					<h1 className="text-xl font-bold">{props.language}</h1>
+				</CardHeader>
+				<CardBody className="flex flex-col gap-3">
+					{showProgress ? (
+						<Progress value={progressValue} />
+					) : (
+						<>
+							<Autocomplete
+								onSelectionChange={() => setDisabled(false)}
+								size="sm"
+								label={`${props.language} Version`}
+							>
+								{versions[props.language.toLowerCase()].map((version) => (
+									<AutocompleteItem key={version}>{version}</AutocompleteItem>
+								))}
+							</Autocomplete>
+							<Button
+								isDisabled={disabled}
+								onPress={() => {
+									setShowProgress(true);
+								}}
+							>
+								Download
+							</Button>
+						</>
+					)}
+				</CardBody>
+			</Card>
+		</div>
+	);
+}
---
+++ src/pages/download.tsx
@@ -0,0 +1,9 @@
+import DownloadCard from "@/components/DownloadCard";
+
+export default function Download() {
+	return (
+		<main>
+			<DownloadCard language="Node"></DownloadCard>
+		</main>
+	);
+}
