7020de11c1979d4213ca9ea87f5abd4a21ce0a9f
Ryan Jasin <github@jasin.dev>
2024-01-28 03:44:30 -0500

    working active highlight

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,30 +6,44 @@ import {
 } from "@heroicons/react/24/outline";
 import Icon from "./Icon";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Sidebar() {
+	const router = useRouter();
 	return (
 		<div className="flex w-64 flex-col justify-between border-r-black bg-transparent bg-fixed p-2">
 			<div className="flex flex-col gap-2">
-				<Link href="#">
-					<Icon active icon={<FolderIcon className="stroke-primary h-5 w-5" />}>
+				<Link href="/">
+					<Icon
+						active={router.pathname == "/"}
+						icon={<FolderIcon className="h-5 w-5 stroke-primary" />}
+					>
 						Projects
 					</Icon>
 				</Link>
-				<Link href="#">
-					<Icon icon={<WrenchIcon className="stroke-primary h-5 w-5" />}>
+				<Link href="/download">
+					<Icon
+						active={router.pathname == "/download"}
+						icon={<WrenchIcon className="h-5 w-5 stroke-primary" />}
+					>
 						Download
 					</Icon>
 				</Link>
-				<Link href="#">
-					<Icon icon={<SparklesIcon className="stroke-primary h-5 w-5" />}>
+				<Link href="/generate">
+					<Icon
+						active={router.pathname == "/generate"}
+						icon={<SparklesIcon className="h-5 w-5 stroke-primary" />}
+					>
 						Generate
 					</Icon>
 				</Link>
 			</div>
 			<div className="">
-				<Link href="#">
-					<Icon icon={<Cog6ToothIcon className="stroke-primary h-5 w-5" />}>
+				<Link href="/settings">
+					<Icon
+						active={router.pathname == "/settings"}
+						icon={<Cog6ToothIcon className="h-5 w-5 stroke-primary" />}
+					>
 						Settings
 					</Icon>
 				</Link>
