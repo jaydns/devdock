7856a67e504b84d6a2cf30b29d30ae597b29d97f
Ryan Jasin <github@jasin.dev>
2024-01-28 03:46:09 -0500

    background

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -2,7 +2,7 @@ import DownloadCard from "@/components/DownloadCard";
 
 export default function Download() {
 	return (
-		<main className="h-screen w-screen p-4">
+		<main className="h-screen w-screen bg-white p-4 dark:bg-black">
 			<div className="flex flex-col items-center justify-center gap-8">
 				<div className="flex flex-col gap-4">
 					<h1 className="text-center text-2xl font-bold">Toolchains</h1>
