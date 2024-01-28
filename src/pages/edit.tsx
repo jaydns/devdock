d997bc4cf1d917643f54d35972115006a99a6505
Ryan Jasin <github@jasin.dev>
2024-01-28 01:51:12 -0500

    edit page

---
+++ src/pages/edit.tsx
@@ -0,0 +1,18 @@
+import { Card, Input, Spacer, Textarea, Button } from "@nextui-org/react";
+
+export default function EditProject() {
+	return (
+		<div className="mx-auto flex h-screen flex-col items-center">
+			<div className="mx-auto mt-4 flex flex-col items-center gap-4">
+				<h1 className="text-center">Project Name</h1>
+				<Input size="sm" label="New Project Title"></Input>
+			</div>
+			<div className="mx-auto mt-6 flex flex-col items-center gap-4">
+				<Textarea label="Description" placeholder="New Description"></Textarea>
+			</div>
+			<Button variant="faded" radius="sm" size="md" className="mt-6">
+				Select New Directory
+			</Button>
+		</div>
+	);
+}
